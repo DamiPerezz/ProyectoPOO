aafb4cda056523d4a8daa21e85b50427e8c370e8
roovedot <ikeralamo2004@gmail.com>
2023-05-23 11:10:49 +0200

    ControlTablero

---
+++ Proyecto/src/Control/ControlTablero.java
@@ -0,0 +1,25 @@
+package Control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Model.*;
+
+public class ControlTablero implements ActionListener{
+	ViewTablero tablero;
+	Tablero t = new Tablero();
+	String palabraCorrecta  
+	
+	public ControlTablero (ViewTablero win) {
+		tablero = win;
+	}
+
+	
+	public void actionPerformed (ActionEvent e) {
+		if(e.getSource() == tablero.probar) {
+			
+			
+		}
+		
+	}
+}
